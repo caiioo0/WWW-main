1f1487fa640bf9fca09bec2a950c57f904909522
caiioo0 <caio.rruda2019@gmail.com>
2023-09-12 20:16:07 -0300

    alterações

---
+++ projetoporto/src/components/header/header.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Header() {
+    return (
+
+        <header>
+            <h1>Challenge Porto</h1>
+            <img src="logo.png" alt="Logo" id="logo"></img>
+            < nav>
+                <ul>
+                    <li><Link href="/">Página inicial</Link></li>
+                    <li><Link href="/ideia">Nossa Ideia</Link></li>
+                    <li><Link href="/organograma">Organograma</Link></li>
+                    <li><Link href="/equipe">Equipe</Link></li>
+                </ul>
+            </nav>
+        </header>
+
+    )
+}
